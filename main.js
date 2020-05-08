a5f5735aa81edc1c74b6f184c8edb2312021795e
f1uk3r <agarwal.umang4321@gmail.com>
2020-05-08 17:21:28 +0530

    blackHover function working

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ function createGrid(gridSize) {
     for (let i = 0; i < gridSize; i++) {
         var rowContainer = document.createElement("div");
         rowContainer.className = "rowContainer" + i;
-        rowContainer.id = i;
         rowContainer.style.display = "flex";
         rowContainer.style.flexWrap = "nowrap";
         rowContainer.style.width = "100%";
@@ -14,12 +13,31 @@ function createGrid(gridSize) {
         document.querySelector(".sketchpad").appendChild(rowContainer);
         for (let j = 0; j < gridBox; j++) {
             var singleBox = document.createElement("div");
+            singleBox.className = "single-box"
+            singleBox.className += " box-" + i + "-" + j
             singleBox.style.width = 100/gridSize + "%";
             singleBox.style.height = "100%";
-            singleBox.style.backgroundColor = "grey"
+            singleBox.style.backgroundColor = "#f6f6f6"
+            singleBox.style.border = "1px solid black";
             document.querySelector(".rowContainer" + i).appendChild(singleBox);
         }
     }
 }
 
 createGrid(gridBox);
+
+function blackHover() {
+    let allBoxes = document.querySelectorAll('.single-box')
+    allBoxes.forEach((box) => {
+        box.addEventListener("mouseenter", (e) => {
+            e.target.style.backgroundColor = "rgb(0,0,0)"
+        })
+    })
+}
+
+blackHover();
+
+function randomColor() {
+    let allBoxes = document.querySelectorAll('.single-box')
+    
+}
